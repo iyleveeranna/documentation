57392cfcb1c69a2ba9f1ea1d4481bb1cf624218e
veeranna-iyle <veeranna.iyle@gmail.com>
2024-11-29 20:10:22 +0530

    update swift docs plugin version

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/apple/swift-docc-plugin",
-            from: "1.0.0")
+            from: "1.4.3")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
